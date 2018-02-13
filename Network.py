4beed6f01d04aea69ed1599989f9c41e3f433e9a
DakotaConnors <djconnors@mavs.coloradomesa.edu>
2018-02-13 09:09:14 -0700

    Add files via upload

--- Network.py
+++ Network.py
@@ -1,10 +1,19 @@
 #http://flask.pocoo.org/docs/0.12/quickstart/
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
-def main():
+@app.route('/')
+def homePage():
+    
     return render_template('index.html')
+
+@app.route('/index',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      print('requesting data')
+      result = request.form
+      print('recieved data',result)
+      return render_template("index.html")
 
 app.run()
